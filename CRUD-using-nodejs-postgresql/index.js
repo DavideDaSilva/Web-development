66cad25b101786b9997b9f5bb3018cff65dc4ef8
DavideDaSilva <silvadavide245@gmail.com>
2024-05-30 14:29:09 -0300

    #5-edit and update button implemented

--- CRUD-using-nodejs-postgresql/index.js
+++ CRUD-using-nodejs-postgresql/index.js
@@ -36,6 +36,7 @@ app.use('/static', express.static('static'))
 app.use(express.urlencoded({extended: true}))
 app.use(express.json())
 
+// READ
 app.get('/', async(req, res) =>{
     const data = await pool.query(`SELECT * FROM todo`);
     res.render('index', {data: data.rows})
@@ -56,11 +57,31 @@ app.post('/addTodo', async(req, res)=>{
     try {
         const result = await pool.query(`INSERT INTO todo (todo, date) VALUES ($1, $2) RETURNING *`, [todo, date])
         // The RETURNING* clause returns all values ​​from the newly inserted row, including all columns.
-        console.log(result)
         res.redirect('/') // REDIRECT TO THE index.ejs page (the root page)
     } catch (error) {
         console.log('Error in adding todo')
         res.status(500).json({errpr: 'Internal Server Error'})
+    }
+})
+
+//UPDATE
+app.get('/edit/:id', async(req, res)=> {
+    const id = req.params.id;
+    const data = await pool.query(`SELECT * FROM todo WHERE id= $1`, [id])
+    res.render('edit', {data: data.rows})
+})
+
+// ENDPOINT FOR UPDATE
+app.post('/update/:id', async(req, res)=> {
+    const id = req.params.id;
+    const {todo, date} = req.body;
+
+    try {
+        await pool.query(`UPDATE todo SET todo = $1, date = $2 WHERE id = $3`, [todo, date, id])
+        res.redirect('/')
+    } catch (error) {
+        console.error('Error while updation todo:', error)
+        res.status(500).json({error: 'Internal Server Error'})
     }
 })
 
